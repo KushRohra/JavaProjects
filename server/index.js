9f43e18bbcf8ed614ce5003223c2a4a51577a3b0
krohra <krohra@cisco.com>
2021-05-25 22:02:30 +0530

    getting profile details now for a user

--- server/index.js
+++ server/index.js
@@ -1,10 +1,26 @@
 const express = require("express");
+const passport = require("passport");
+const GoogleStatergy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send({"hi": "Index"});
-})
+passport.use(new GoogleStatergy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: "/auth/google/callback"
+}, (accessToken, refreshToken, profile, done) => {
+    console.log(accessToken);
+    console.log(refreshToken);
+    console.log(profile);
+    console.log(done);
+}));
+
+app.get("/auth/google", passport.authenticate('google', {
+    scope: ['profile', 'email']
+}));
+
+app.get("/auth/google/callback", passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()  => {
