24288ab140061a4444aaceba5e056d61f206c300
krohra <krohra@cisco.com>
2021-05-30 22:45:58 +0530

    added header on the landing page on client side

--- server/services/passport.js
+++ server/services/passport.js
@@ -21,18 +21,11 @@ passport.use(new GoogleStatergy({
     clientSecret: keys.googleClientSecret,
     callbackURL: "/auth/google/callback",
     proxy: true
-}, (accessToken, refreshToken, profile, done) => {
-    User.findOne({ googleId: profile.id })
-        .then((existingUser) => {
-            if (existingUser) {
-                // we already have a user with the profile ID
-                done(null, existingUser);
-            } else {
-                // we dont have a record, make a new record
-                new User({ googleId: profile.id })
-                    .save()
-                    .then(user => done(null, user));
-            }
-        })
-    console.log(profile.id);
+}, async (accessToken, refreshToken, profile, done) => {
+    const existingUser = await User.findOne({ googleId: profile.id })
+    if (existingUser) {
+        return done(null, existingUser);
+    } 
+    const user = await new User({ googleId: profile.id }).save()
+    done(null, user)
 }));
